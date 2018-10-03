cf5759c6b81a831814a14efa59775245fe812921
hammer798 <ibradley798@gmail.com>
2018-10-03 14:31:01 -0700

    Updates to Network
    second constructor, findHead and findTail(someone make findTail better pls)

--- Network.java
+++ Network.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 
 public class Network {
-	Activity tail;
+	private Activity head, tail;
+	private Activity[] activities;
 		
 		public  Network(String n, int d, Activity[] p) {
 			tail = new Activity(n, d, null);
@@ -8,6 +10,13 @@ public class Network {
 			tail.previous = null;
 			
 		}
+		
+		public Network(Activity[] activities) {
+			this.activities = activities;
+			findHead();
+			findTail();
+		}
+		
 		public void add(Activity n) {
 			Activity currentActivity = tail;
 			
@@ -22,5 +31,34 @@ public class Network {
 			
 			
 		}
+		
+		public void findHead() {
+			for(int x = 0; x < activities.length; x++) {
+				if(activities[x].getPredecessors() == null || activities[x].getPredecessors().length < 1) {
+					this.head = activities[x];
+				}
+			}
+		}
+		
+		public void findTail() {
+			ArrayList<Activity> temp = new ArrayList<Activity>();
+			for(int x = 0; x < activities.length; x++) {
+				Activity current = activities[x];
+				temp.add(current);
+				for(int y = 0; y < current.getPredecessors().length; y++) {
+					for(int z = 0; z < activities.length; z++) {
+						if(temp.get(z).getName().equals(current.getName())) {
+							temp.remove(z);
+							if(temp.size() == 1) {
+								tail = temp.get(0);
+								return;
+							}
+						}
+						
+					}
+				}
+			}
+		}
+		
 	
 }
