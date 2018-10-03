eb05f09a5f3bce400d9228c46145bb990de5dfc3
Cole Stanfield <colestanfield@coles-mbp.3520.dhcp.asu.edu>
2018-10-03 13:55:34 -0700

    Test

--- Network.java
+++ Network.java
@@ -1,4 +1,26 @@
 
 public class Network {
-
+	Activity tail;
+		
+		public  Network(String n, int d, Activity[] p) {
+			tail = new Activity(n, d, null);
+			tail.next = null;
+			tail.previous = null;
+			
+		}
+		public void add(Activity n) {
+			Activity currentActivity = tail;
+			
+			while(currentActivity.previous != null) {
+				currentActivity = currentActivity.previous;
+			}
+			
+			
+			n.next = currentActivity;
+			currentActivity.previous = n;
+			
+			
+			
+		}
+	
 }
